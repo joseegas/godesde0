6f0955b7010fcedfe89094aa0e63b9017505f5c3
Jose Egas <109235867+joseegas@users.noreply.github.com>
2023-07-14 11:45:34 -0400

    siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"godesde0/variables"
 )
 
 func main(){
 	variables.MuestroEnteros()
+	variables.RestoVariables()
+	estado,texto:=variables.ConviertoaTexto(1555)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
